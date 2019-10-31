0bcbf64fb8c9519f3a62ac332a514a98d34a7846
hss-smee <seanmee8186@gmail.com>
2019-10-31 08:53:54 -0700

    adding ow heroes fixing portfolio link

---
+++ ow-heroes/precache-manifest.ea8b87ec6a04645417ddd1e599f43984.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa956984d703b22e11bd7d25d4b91ec6",
+    "url": "/ow-heroes/index.html"
+  },
+  {
+    "revision": "e0653b9bc1f3b8a47145",
+    "url": "/ow-heroes/static/css/main.e8a48eff.chunk.css"
+  },
+  {
+    "revision": "c3aac0b7c2b492385f60",
+    "url": "/ow-heroes/static/js/2.1f52f17f.chunk.js"
+  },
+  {
+    "revision": "e0653b9bc1f3b8a47145",
+    "url": "/ow-heroes/static/js/main.bc71e954.chunk.js"
+  },
+  {
+    "revision": "078a8ae381e642aca9f7",
+    "url": "/ow-heroes/static/js/runtime-main.223ae007.js"
+  },
+  {
+    "revision": "ff78c5e2b6c3846035dae28cdaa65583",
+    "url": "/ow-heroes/static/media/big_noodle_titling.ff78c5e2.ttf"
+  },
+  {
+    "revision": "52629e481c4d4df7a9783ed08bdecdef",
+    "url": "/ow-heroes/static/media/big_noodle_titling_oblique.52629e48.ttf"
+  }
+]);
